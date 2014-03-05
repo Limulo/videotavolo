eabd10fc42f267d1930d34d9e7353cd05dd2a67d
ariutti <nicola@limulo.net>
2014-03-05 08:21:37 +0100

    aggiunto il file di icona e la dicitura sulla licenza

--- src/Digit.h
+++ src/Digit.h
@@ -1,11 +1,32 @@
 /*
- *  Digit.h
- *  Prototipo3_v6
+ * videoTavolo
+ * ===========
  *
- *  Created by Nicola Ariutti on 19/10/13.
- *  Copyright 2013 Limulo. All rights reserved.
+ * INFO
+ * ===========
+ * videoTavolo è un prototipo di superficie interattiva. 
+ * Vengono qui rilasciati i codici sorgente e le patches di PureData del progetto. 
+ * Il progetto è curato da Limulo ( http://www.limulo.net ) con i seguenti contributi esterni:
  *
+ * 1) ofxPd
+ * Copyright (c) Dan Wilcox 2011-2013
+ * BSD Simplified License.
+ * https://github.com/danomatika/ofxPd
+ * 
+ * 2) ofxTuio
+ * permette di creare e gestire direttamente nell'ambiente 
+ * di sviluppo di openFrameworks un server e un client che 
+ * comunicano tramite protocollo TUIO
+ * https://github.com/patriciogonzalezvivo/ofxTuio
+ * 
+ * LICENZA
+ * ===========
+ * ad eccezione degli elementi elencati qui sopra, tutto il codice è rilasciato da Limulo secondo la licenza 
+ * Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0). Per prendere visione di una copia 
+ * di tale licenza visitate http://creativecommons.org/licenses/by-sa/4.0/ .
+ * 
  */
+
 #ifndef _DIGIT_INIT
 #define _DIGIT_INIT
 
--- src/Fid_Base.h
+++ src/Fid_Base.h
@@ -1,4 +1,32 @@
 /*
+ * videoTavolo
+ * ===========
+ *
+ * INFO
+ * ===========
+ * videoTavolo è un prototipo di superficie interattiva. 
+ * Vengono qui rilasciati i codici sorgente e le patches di PureData del progetto. 
+ * Il progetto è curato da Limulo ( http://www.limulo.net ) con i seguenti contributi esterni:
+ *
+ * 1) ofxPd
+ * Copyright (c) Dan Wilcox 2011-2013
+ * BSD Simplified License.
+ * https://github.com/danomatika/ofxPd
+ * 
+ * 2) ofxTuio
+ * permette di creare e gestire direttamente nell'ambiente 
+ * di sviluppo di openFrameworks un server e un client che 
+ * comunicano tramite protocollo TUIO
+ * https://github.com/patriciogonzalezvivo/ofxTuio
+ * 
+ * LICENZA
+ * ===========
+ * ad eccezione degli elementi elencati qui sopra, tutto il codice è rilasciato da Limulo secondo la licenza 
+ * Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0). Per prendere visione di una copia 
+ * di tale licenza visitate http://creativecommons.org/licenses/by-sa/4.0/ .
+ * 
+ */
+/*
  *  Fid_Base.h
  *  Prototipo3
  *
